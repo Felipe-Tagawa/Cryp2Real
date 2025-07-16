17ccdf0251a0a6ec41ad0ba4b4aa2006c8d98619
Felipe Tagawa <137126826+Felipe-Tagawa@users.noreply.github.com>
2025-07-16 09:57:29 -0300

    Update app.py

--- Backend/FlaskProject/Backend/app.py
+++ Backend/FlaskProject/Backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from Backend.FlaskProject.Backend.deploy_contract import receipt
 from Backend.FlaskProject.Backend.deploy_output import sistema_cliente_address, new_ether_address, sistema_cliente_abi, new_ether_abi
 from Backend.FlaskProject.Backend.utils import sign_n_send, listAllAccounts, get_eth_to_brl
-from Backend.FlaskProject.Backend.blockchain import w3, admWallet, private_key, merchantWallet
+from Backend.FlaskProject.Backend.my_blockchain import w3, admWallet, private_key, merchantWallet
 import time
 
 if w3.is_connected():
