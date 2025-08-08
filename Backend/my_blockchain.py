1c09e40a040a0b180d05ea26f9e0db47efa4ccec
Seu Nome <OKamizura@users.noreply.github.com>
2025-08-08 19:16:48 -0300

    Caminho arrumado

--- Backend/my_blockchain.py
+++ Backend/my_blockchain.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GANACHE_URL = "HTTP://127.0.0.1:7545"
+GANACHE_URL = "http://127.0.0.1:7545"
 PRIVATE_KEY = "0x1f9f1feb9dd4a6fa9f7de741de664b2e41e834a7b9be63a04e1ae2ed8ee56a42"
 
 w3 = Web3(Web3.HTTPProvider(GANACHE_URL))
 
 account = w3.eth.accounts
 
-admWallet = account[0]  # exemplo: conta admin
-ongWallet = account[2]  # exemplo: conta ONG
+admWallet = account[0]  # conta admin
+ongWallet = account[2]  # conta ONG
 
 # Criar nova conta para comerciante
 nova_conta = Account.create()
 merchantWallet = nova_conta.address
 private_key_comerciante = nova_conta.key.hex()
 
-private_key = PRIVATE_KEY  # sua chave privada, preferencialmente carregada do .env
+private_key = PRIVATE_KEY
